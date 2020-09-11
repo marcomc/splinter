f4b95b4796eb8b2405a2baaf70d3ff453e8ceae2
Marco Massari Calderone <marco@marcomc.com>
2020-09-11 15:07:20 -0300

    add execute function

--- install.sh
+++ install.sh
@@ -5,6 +5,22 @@ set -u
 SPLINTER_DIR="splinter"
 SPLINTER_REPO="https://github.com/marcomc/splinter"
 
+shell_join() {
+  local arg
+  printf "%s" "$1"
+  shift
+  for arg in "$@"; do
+    printf " "
+    printf "%s" "${arg// /\ }"
+  done
+}
+
+execute() {
+  if ! "$@"; then
+    abort "$(printf "Failed during: %s" "$(shell_join "$@")")"
+  fi
+}
+
 echo "Downloading Splinter in to '${SPLINTER_DIR}'..."
 (
   # we do it in four steps to avoid merge errors when reinstalling
